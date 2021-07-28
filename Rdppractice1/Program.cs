965132b3ca838d0c9d58397926aa1d762ca0a4fe
lipsa7 <lipsakar98@gmail.com>
2021-07-28 12:02:37 +0530

    Add project files.

---
+++ Rdppractice1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdppractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(UserProgramCode.sumOfSquares(n));
+
+            Console.ReadKey();
+        }
+    }
+
+    class UserProgramCode
+    {
+        public static int sumOfSquares(int n)
+        {
+            if (n <= 0)
+            {
+                return -1;
+            }
+            int sum = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                sum += (int)Math.Pow(i, 2);
+            }
+            return sum;
+        }
+    }
+}
